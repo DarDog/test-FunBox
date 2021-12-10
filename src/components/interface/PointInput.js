9d16f729d0a81de0a8cff69134142ec20f769d30
DarDog <samediacc666@gmial.com>
2021-12-10 16:54:09 +1100

    Feat: add handle input changes

--- src/components/interface/PointInput.js
+++ src/components/interface/PointInput.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PointInput = () => {
+  const [pointName, setPointName] = useState('')
+
+  const handleChange = (e) => {
+    setPointName(e.target.value)
+  }
+
   return (
     <article className="container">
       <form className="form">
-        <input type="text" className="form__input"/>
+        <input onChange={handleChange} type="text" className="form__input"/>
       </form>
     </article>
   );
