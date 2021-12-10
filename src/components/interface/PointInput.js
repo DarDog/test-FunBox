dbed4f22c7efb3a5b9a3825f62f83201c8ce9e09
DarDog <samediacc666@gmial.com>
2021-12-10 16:58:35 +1100

    Feat: add handle form submit

--- src/components/interface/PointInput.js
+++ src/components/interface/PointInput.js
@@ -3,14 +3,21 @@ import React, {useState} from 'react';
 const PointInput = () => {
   const [pointName, setPointName] = useState('')
 
-  const handleChange = (e) => {
-    setPointName(e.target.value)
+  const handleChange = evt => {
+    setPointName(evt.target.value)
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+
+    setPointName('')
+      //TODO создавать объект точки с уникальным ID и отправлять в список точек
   }
 
   return (
     <article className="container">
-      <form className="form">
-        <input onChange={handleChange} type="text" className="form__input"/>
+      <form onSubmit={handleSubmit} className="form">
+        <input value={pointName} onChange={handleChange} type="text" className="form__input"/>
       </form>
     </article>
   );
