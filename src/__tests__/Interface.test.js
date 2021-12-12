8f27ded550d1b9eb1c57ada7efccf6e1620f7b59
DarDog <samediacc666@gmial.com>
2021-12-12 20:37:04 +1100

    style: remove unused code

--- src/__tests__/Interface.test.js
+++ src/__tests__/Interface.test.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Interface from '../components/interface/Interface';
-import '@testing-library/jest-dom'
 
 const points = [
   {
